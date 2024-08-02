06872967ec0991e0a3983cefd01893e0bf8fcc5e
GjertsenValentin <2301352208@qq.com>
2024-08-02 13:07:55 +0800

    引用变量的使用（1）2024.8.2

--- GjertsenValentin.cpp
+++ GjertsenValentin.cpp
@@ -2,6 +2,26 @@
 int main()
 {
     using namespace std;
-    cout<<"hello!";
-    return 0;
+    int rats=101;
+    int *pt=&rats;
+    int &rodents=*pt;
+    int bunnies=50;
+    pt=&bunnies;
+    cout<<"*pr = "<<*pt;
+    cout<<"rodents = "<<rodents;
+    //从上面的例子我们可以看出来，rodents虽然通过指针才指向rats，当指针改变的时候，rodents并没有改变证明，引用变量不会改变他的指向。
+//    int rats=101;
+//    int &rodents=rats;
+//    cout<<"rats = "<<rats;
+//    cout<<", rodents = "<<rodents<<endl;
+//
+//    int bunnies = 50;
+//    rodents = bunnies;
+//    cout<<"bunnies = "<<bunnies;
+//    cout<<", rats = "<<rats;
+//    cout<<", rodents = "<<rodents<<endl;
+
+//    cout<<"bunniesaddress = "<<&bunnies;
+//    cout<<", rodents address = "<<&rodents<<endl;
+//    return 0;
 }
